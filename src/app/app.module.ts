84b4f725ecca6e5fbcd8086adb223b10108c1463
Swaroop N <swaroopn96@gmail.com>
2022-10-11 20:09:22 +0530

    Included Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    // FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
