19a13c4b5f2031fc6e04c0782d43722a3b6effb2
Swaroop N <swaroopn96@gmail.com>
2023-01-02 20:51:28 +0530

    Styling elements dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,8 +8,16 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'online';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   //Method
   getServerStatus() {
-    this.serverStatus;
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
