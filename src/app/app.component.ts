9a887ab29bd40257946bbfba10eaed1359b9c666
Swaroop N <swaroopn96@gmail.com>
2023-01-04 22:17:59 +0530

    Understanding Angular Error Messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
+  //   styles: [
+  //     `
+  //       h3 {
+  //         color: dodgerblue;
+  //       }
+  //     `,
+  //   ],
 })
 export class AppComponent {
-  title = 'my-app';
-  name = 'Swaroop';
+  // title = 'my-app';
+  // name = 'Swaroop';
+  servers;
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    const position = id + 1;
+    // splice Removes element from the given position
+    this.servers.splice(position, 1);
+  }
 }
