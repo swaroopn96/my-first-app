2d127d008e7fc672647fa6343a7a15bf3c6de50f
Swaroop N <swaroopn96@gmail.com>
2023-01-07 00:02:53 +0530

    Assigned an Alias to Custom Property

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -7,7 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   // Used @Input decorator to access this element outside this component i.e in app component
-  @Input() element: { type: string; name: string; content: string };
+  //srvElement is the alias name to bind property
+  @Input('srvElement') element: { type: string; name: string; content: string };
   constructor() {}
 
   ngOnInit(): void {}
