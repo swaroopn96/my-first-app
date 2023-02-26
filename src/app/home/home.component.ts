f87b5c24f76f97a50dc82e531b7bc523d4113b73
Swaroop N <swaroopn96@gmail.com>
2023-02-27 00:59:30 +0530

    Created Observable with unsubcription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,15 +30,26 @@
 //   }
 // }
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    //Fires even every 1 sec
+    this.firstObsSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
